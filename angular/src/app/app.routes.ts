3cd1f52e643584f51605e0e9bb24f4c6c44c9081
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 15:52:36 +0100

    added routing for the iframes

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { IframeGoogleMapComponent } from './iframe-google-map/iframe-google-map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'google-map',  component: IframeGoogleMapComponent },
+];
