a5b6d6b0bc59382c642a9aa5e00ab23f429ffd01
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 19:17:45 +0100

    removed dead component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { IframeAppComponent } from './iframe-app/iframe-app.component';
 import { IframeGoogleMapComponent } from './iframe-google-map/iframe-google-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, IframeAppComponent, IframeGoogleMapComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, IframeGoogleMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
