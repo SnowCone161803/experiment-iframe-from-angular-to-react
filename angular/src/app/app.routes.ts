526e2a1ce0fd5b885fdd707adc1ea669ac815fc4
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 15:55:10 +0100

    added local iframe component

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { IframeGoogleMapComponent } from './iframe-google-map/iframe-google-map.component';
+import { LocalIframeComponentComponent } from './local-iframe-component/local-iframe-component.component';
 
 export const routes: Routes = [
   {path: 'google-map',  component: IframeGoogleMapComponent },
+  {path: 'local-iframe',  component: LocalIframeComponentComponent },
 ];
--- angular/src/app/local-iframe-component/local-iframe-component.component.ts
+++ angular/src/app/local-iframe-component/local-iframe-component.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-local-iframe-component',
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './local-iframe-component.component.html',
   styleUrl: './local-iframe-component.component.css'
 })
-export class LocalIframeComponentComponent {
+export class LocalIframeComponentComponent implements OnInit {
 
+  @ViewChild('iframeref')
+  iframeRef: any
+
+  currentUrl: string = "<unknown>";
+
+  async ngOnInit() {
+    await new Promise( resolve => setTimeout(resolve, 1000) );
+    const source = this.iframeRef.nativeElement
+    console.log(`iframeref src:`, source)
+    this.currentUrl = this.iframeRef.nativeElement.src
+  }
 }
