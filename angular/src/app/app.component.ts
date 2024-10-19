c739c5ef8edae7112b81a59d01d8d3672ba50ce8
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 14:55:26 +0100

    now have iframe with map contents

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IframeAppComponent } from './iframe-app/iframe-app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IframeAppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
