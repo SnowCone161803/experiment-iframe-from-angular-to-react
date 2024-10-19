ad6f576b4dee9125f42be060984368fae23b8761
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 15:37:22 +0100

    now displaying source URL from the iframe

--- angular/src/app/iframe-google-map/iframe-google-map.component.ts
+++ angular/src/app/iframe-google-map/iframe-google-map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-iframe-google-map',
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './iframe-google-map.component.html',
   styleUrl: './iframe-google-map.component.css'
 })
-export class IframeGoogleMapComponent {
+export class IframeGoogleMapComponent implements OnInit {
 
+  @ViewChild('iframeref')
+  iframeRef: any
+
+  currentUrl: string = "<unknown>";
+
+  async ngOnInit() {
+    await new Promise( resolve => setTimeout(resolve, 1000) );
+    const source = this.iframeRef.nativeElement
+    console.log(`iframeref src:`, source)
+    this.currentUrl = this.iframeRef.nativeElement.src
+  }
 }
