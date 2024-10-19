fef973eafe0395b93a87caec2d66d32dfcb23db6
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 14:46:55 +0100

    added iframe app component

---
+++ angular/src/app/iframe-app/iframe-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iframe-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './iframe-app.component.html',
+  styleUrl: './iframe-app.component.css'
+})
+export class IframeAppComponent {
+
+}
