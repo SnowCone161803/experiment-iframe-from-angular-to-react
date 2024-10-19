db22b3994421d7b587296a57620e3a3260302158
Gordon Macrae <macraegordon@hotmail.com>
2024-10-19 16:26:09 +0100

    now able to pass values from the parent to the child

--- other-angular/src/app/app.component.ts
+++ other-angular/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'other-angular';
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  get fromParent(): Observable<string> {
+    this.route.queryParams.subscribe(console.log)
+    const par = this.route.queryParams.pipe(
+      map(p => p['fromParent']),
+    )
+    par.subscribe(console.log)
+    return par
+  }
 }
