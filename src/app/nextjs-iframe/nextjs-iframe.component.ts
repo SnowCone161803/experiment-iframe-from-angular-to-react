3183a2b4b8170a990a9586c47697cb969978ffb7
Gordon Macrae <macraegordon@hotmail.com>
2024-10-20 17:53:36 +0100

    added content to nextjs in iframe component

--- src/app/nextjs-iframe/nextjs-iframe.component.ts
+++ src/app/nextjs-iframe/nextjs-iframe.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-nextjs-iframe',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './nextjs-iframe.component.html',
   styleUrl: './nextjs-iframe.component.css'
 })
 export class NextjsIframeComponent {
 
+  @ViewChild('iframeref')
+  iframeRef: any
+
+  currentUrl: string = "<unknown>";
+  messageFromChild = new BehaviorSubject<string>("<none>")
+  idFromChild = new BehaviorSubject<string>("<none>")
+  count: number = 0
+
+  iframeReady() {
+    const source = this.iframeRef.nativeElement
+    console.log("iframe source", source)
+    this.currentUrl = this.iframeRef.nativeElement.src
+
+    const contentWindow = this.iframeRef.nativeElement.contentWindow
+    contentWindow.addEventListener("message", async (event: MessageEvent) => {
+      let data = event.data
+      if (typeof(event.data) === 'string') {
+        data = JSON.parse(event.data)
+      }
+      if (data.target !== 'parent') {
+        return
+      }
+      console.log("message received by parent", data)
+      this.messageFromChild.next(JSON.stringify(data))
+    })
+  }
+
+  postMessageToChild() {
+    this.iframeRef.nativeElement.contentWindow.postMessage({
+      from:'parent',
+      message: 'from parent ',
+      target: 'child',
+      count: this.count,
+    })
+    ++this.count
+  }
 }
